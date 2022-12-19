61562d3059fe88c61c5efa7fe50644bfda8308d3
Vu Duy Tu <tu.vuduy@vti.com.vn>
2022-12-19 14:42:25 +0700

    test circleCI

--- scripts/after_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/Relic-poc-test-app-rails-cd-v1/deploy.log
-
-cd /home/ec2-user/Relic-poc-test-app-rails-cd-v1 >> /home/ec2-user/Relic-poc-test-app-rails-cd-v1/deploy.log
--- scripts/before_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo rm -rf /home/ec2-user/Relic-poc-test-app-rails-cd-v1/.gitignore
-sudo rm -rf /home/ec2-user/Relic-poc-test-app-rails-cd-v1/.ruby-version
-sudo rm -rf /home/ec2-user/Relic-poc-test-app-rails-cd-v1/*
